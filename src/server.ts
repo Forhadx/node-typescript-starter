6bf3cf17f6a8cbe04071abb51a3a45f48b319d35
forhadx <forhadsh1@gmail.com>
2023-07-22 00:07:09 +0600

    add route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("hi");
+import express, { Request, Response, NextFunction, Application } from "express";
+import { Server } from "http";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("hello");
+});
+
+const server: Server = app.listen(5000, () => console.log("run at 5000"));
